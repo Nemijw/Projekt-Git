079d40a85605ea66407b2d0314face2db1dcc250
Simon Dicander <Dicander.simon@gmail.com>
2022-06-13 10:28:18 +0200

    Added script for show/hide bulb

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("bulb1").addEventListener("click", function(){
+    var bulb1img = document.getElementById("bulb1img");
+    if(bulb1img.style.display == "none"){
+        bulb1img.style.display = "block";
+    }else{
+        bulb1img.style.display = "none";
+    }
+});
+
+document.getElementById("bulb2").addEventListener("click", function(){
+    var bulb1img = document.getElementById("bulb2img");
+    if(bulb1img.style.display == "none"){
+        bulb1img.style.display = "block";
+    }else{
+        bulb1img.style.display = "none";
+    }
+});
+
+document.getElementById("bulb3").addEventListener("click", function(){
+    var bulb1img = document.getElementById("bulb3img");
+    if(bulb1img.style.display == "none"){
+        bulb1img.style.display = "block";
+    }else{
+        bulb1img.style.display = "none";
+    }
+});
